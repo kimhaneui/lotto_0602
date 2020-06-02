369e10fcc5dc9b9459b8fd61ca458328c2f576f9
kimhaneui <kimhaneui2@naver.com>
2020-06-02 10:16:20 +0900

    당첨번호 6개 뽑기

--- app/src/main/java/com/example/lotto_0602/MainActivity.java
+++ app/src/main/java/com/example/lotto_0602/MainActivity.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.example.lotto_0602.databinding.ActivityMainBinding;
+
+import java.util.Arrays;
 
 public class MainActivity extends BaseActivity {
 
     ActivityMainBinding binding;
+
+    int[] winLottoNumber = new int[6];
+    int bonusNum = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +29,43 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupEvents() {
 
+        binding.buyoneLottoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeLottoWinNumbers();
+            }
+        });
     }
 
     @Override
     public void setValues() {
 
+    }
+    void  makeLottoWinNumbers(){
+        for (int i=0;i<winLottoNumber.length;i++){
+            winLottoNumber[i]=0;
+        }
+        for(int i =0 ; i<winLottoNumber.length;i++){
+            while (true){
+                int randomNum = (int) (Math.random()*45+1);
+                boolean isDuplicatedOk = true;
+                for (int num:winLottoNumber){
+                    if(num==randomNum){
+                        isDuplicatedOk = false;
+                        break;
+                    }
+                }
+                if(isDuplicatedOk){
+                    winLottoNumber[i]=randomNum;
+                    break;
+                }
+            }
+        }
+
+        Arrays.sort(winLottoNumber);
+
+        for (int winNum:winLottoNumber){
+            Log.d("당첨번호",winNum+"");
+        }
     }
 }
